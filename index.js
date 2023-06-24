71da380335622247cd38880f0b32349a2db601e4
Katie Sayers <kate@student.dev-island.org>
2023-06-23 14:48:59 -1000

    First commit of UI addition

--- index.js
+++ index.js
@@ -2,22 +2,37 @@ let roundCount = 0;
 let playerScore = 0; 
 let compScore = 0; 
 
-while (roundCount < 5) {
+let playerText = document.querySelector('div.player p');
+let compText = document.querySelector('div.comp p');
+let roundText = document.querySelector('div.round-count p');
+
+playerText.textContent = `Your Score: 0`;
+compText.textContent = `Computer's Score: 0`;
+roundText.textContent = `Round Count:`;
 
 //get Player's bet:
-let playerInput = prompt("Enter your choice: rock, paper or scissors").toLowerCase();
+let playerInput = '';
+let rock = document.querySelector("button.rock");
+let paper = document.querySelector("button.paper");
+let scissors = document.querySelector("button.scissors");
 
-//make sure that they enter a valid choice:
-while (playerInput !== "rock" && playerInput !== "paper" && playerInput !== "scissors") {
-    playerInput = prompt("Enter your choice: rock, paper or scissors")
-};
+rock.addEventListener("click", () => {
+   console.log("Player's Choice: ", playerInput = 'rock');
+   startGame();
+});
+paper.addEventListener("click", () => {
+    console.log("Player's Choice: ", playerInput = 'paper');
+    startGame();
+});
+scissors.addEventListener("click", () => {
+    console.log("Player's Choice: ", playerInput = 'scissors');
+    startGame();
+});
 
-console.log("Player's Choice: ", playerInput);
+const startGame = () => {
 
 //get Computer's bet:
 let compRandom = Math.floor((Math.random() * 3) + 1)
-
-// console.log(compRandom);
 
 //convert random number into a valid choice:
 let compBet = () => {
@@ -29,14 +44,13 @@ if (compRandom === 1) {
     return "scissors"
 }
 }
-console.log("Computer's Choice: ", compBet());
 
 //play round:
 let playRound = (player, computer) => {
     if (player === computer) {
         compScore++;
         playerScore++;
-        return "Tie";
+        return "Tie"
     } else if (player === "rock" && computer === "scissors") {
         playerScore++;
         return "You win! Rock beats scissors."
@@ -56,7 +70,7 @@ let playRound = (player, computer) => {
         compScore++;
         return "You lose. Rock beats scissors."
     }
-}
+};
 
 let endOfGame = () => {
     if (playerScore > compScore) {
@@ -66,21 +80,20 @@ let endOfGame = () => {
     } else {
         return "GAME OVER. It's a tie!"
     }
-}
+};
+
+playRound(playerInput, compBet());
 
 roundCount++;
 
-console.log(playRound(playerInput, compBet()));
-console.log("Player's Score: ", playerScore);
-console.log("Computer's Score: ", compScore);
-console.log("Round: ", roundCount);
-console.log("--------------------");
+playerText.textContent = `Your Score: ${playerScore}`;
+compText.textContent = `Computer's Score: ${compScore}`;
+roundText.textContent = `Round Count: ${roundCount}`;
 
 if (roundCount === 5) {
     console.log(endOfGame());
     }
 }
-
 
 
 
